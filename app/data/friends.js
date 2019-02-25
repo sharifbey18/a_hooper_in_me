4eaae3905284d0b523c39f45572714ea162220ae
Joel Brown <joelbrown90@gmail.com>
2019-02-25 01:18:50 -0500

    hooper

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [{
+    "name": "Harden",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-1ecnT8oVQ2Zgk2-71l8y4mAQmmqjjLkQw2zOmZdQhqkJEP07",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Lonzo",
+    "photo": "https://cdn.images.express.co.uk/img/dynamic/4/590x/Lonzo-Ball-1090726.jpg?r=1550880912188",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Lebron",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtdZXaIsyNHaN-b_7AELvLmCazgNOI-0wnZ4YBGBdPiym5yjTo7g",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}, {
+    "name": "",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3pnimIjaAuSSfxwpxw97kHDPe0WDIwmL_ncDZ9zTaem3nuwWTtA",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "Wade",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNX-R6OsBCtg7hDDm7vhCid-rFwdctKZDowQYZYt_gT7T2qwwW",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "PG13",
+    "photo": "https://cdn.images.express.co.uk/img/dynamic/4/590x/George-1091258.jpg?r=1550904681507",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Kyrie",
+    "photo": "http://images.performgroup.com/di/library/omnisport/3c/8a/kyrie-irving-usnews-011619-ftr-gettyjpg_1hoi26x7jzj0417l3ndruk9pm7.jpg?t=1508209224&w=960&quality=70",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "AD",
+    "photo": "https://nesncom.files.wordpress.com/2019/02/anthony-davis-4.jpg",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Curry",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkXRG3AfbICBbIUWoyz5XIxkV1t40sXRyRgcKLBqq_jBK51QNo9A",
+    "scores": [1, 5, 1, 3, 5, 1, 4, 3, 2, 1]
+}, {
+    "name": "KD",
+    "photo": "https://b.fssta.com/uploads/2017/12/kdsizzle.vresize.1200.630.high.48.jpg",
+    "scores": [1, 3, 3, 1, 2, 4, 4, 1, 3, 1]
+}, {
+    "name": "Ben",
+    "photo": "https://usatthebiglead.files.wordpress.com/2018/05/gettyimages-954394622.jpg?w=1000&h=600&crop=1?w=1000&crop=0",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "CP3",
+    "photo": "https://www.nba.com/rockets/sites/rockets/files/getty-images-944487630.jpg",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}, {
+    "name": "Russ",
+    "photo": "https://static01.nyt.com/images/2016/08/05/sports/05WESTBROOKWEB/05WESTBROOKWEB-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    "scores": [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+}, {
+    "name": "Giannis",
+    "photo": "https://heightline.com/wp-content/uploads/giannis-640x400.png",
+    "scores": [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+}];
+
+module.exports = friends;
